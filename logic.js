b4f841f2ca9f9b5fa281effe6b19933c0fdfc8ff
Hatsunmikk <debos2003@gmail.com>
2024-12-02 20:04:12 +0530

    Add main game logic within playGame() for five rounds. Add the previous functions within playGame()

--- logic.js
+++ logic.js
@@ -1,48 +1,64 @@
 
 //Main function to play the game
-//Function to get a random choice from the computer
-function getComputerChoice(){
+function playGame(){
+
+    //Variables to keep track of the scores
+     let compScore = 0;
+     let humanScore = 0;
+
+     
+    //Function to get a choice from the human
+    function getHumanChoice(){
+    let humanChoice = prompt("Rock, Paper or Scissors?");
+
+    if(humanChoice === null){              //when user cancels or closes the prompt
+        alert("You cancelled the game.");
+        return null;
+    };
+
+    humanChoice = humanChoice.toLowerCase();
+
+    if(humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors"){
+        alert(`You chose ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}.`);
+        return humanChoice;
+        
+       
+    } else {
+        alert("Please choose Rock, Paper or Scissors!");
+        return getHumanChoice(); 
+        
+    }
+    
+    
+};
+
+    //Function to get a random choice from the computer
+      function getComputerChoice(){
     let computerChoice = Math.random();
     
     if(computerChoice < 0.37){
-        computerChoice = "Rock";
+        computerChoice = "rock";
     } else if (computerChoice <= 0.67){
-        computerChoice = "Paper";
+        computerChoice = "paper";
     } else {
-        computerChoice = "Scissors";
-    }
+        computerChoice = "scissors";
+    };
+
+    alert(`The computer chose ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}.`);
 
     return computerChoice;
-};
-
-//Function to get a choice from the human
-function getHumanChoice(){
-    let humanChoice = prompt("Rock, Paper or Scissors?");
-
-    if(humanChoice === "Rock"){
-        alert("You chose Rock.")
-    } else if (humanChoice === "Paper"){
-        alert("You chose Paper.")
-    } else if (humanChoice === "Scissors"){
-        alert("You chose Scissors.")
-    } else {
-        alert("Please choose Rock, Paper or Scissors!");
-        getHumanChoice();  //Using recursion to guide the user back to the initial prompt if they enter an invalid choice
-    }
-
+   
+   
     
 };
 
-//Variables to keep track of the scores
-let compScore = 0;
-let humanScore = 0;
+
+
 
 //Function for each round
 function playRound (humanChoice, computerChoice){
 
-    humanChoice = humanChoice.toLowerCase();
-    computerChoice = computerChoice.toLowerCase();
-
+   
     //cases where the human wins
     if(humanChoice === "rock" && computerChoice === "scissors"){
         alert("You win! Rock beats Scissors.");
@@ -65,13 +81,63 @@ function playRound (humanChoice, computerChoice){
         compScore += 1;
     } else if(humanChoice === computerChoice){   //cases of tie
         alert("It's a tie! Break it by choosing again.");
-        getHumanChoice();  //if there is a tie, break it by getting a new choice from the human, which starts a fresh round
+         return null;
     }
+
+    return {humanScore, compScore};
        
+};
+
+//Main game logic:
+
+for(let round = 0; round < 5; round++){
+ 
+    //Variables that will act as parameters for playRound()
+    const humanSelection = getHumanChoice();
+
+    if(humanSelection === null){
+        break;
+    };
+
+    const compSelection = getComputerChoice();
+
+    const roundResult = playRound(humanSelection, compSelection);
+    if(roundResult === null){
+        round--;
+        continue;
+    }
+
+    humanScore = roundResult.humanScore;
+    compScore = roundResult.compScore;
+
+    if(humanScore > compScore){
+        alert(`You win! \n Human: ${humanScore} \n Computer: ${compScore}`);
+    } else if (compScore > humanScore){
+        alert(`Computer wins! \n Human: ${humanScore} \n Computer: ${compScore}`)
+    };
+  
+};
+
+//Deciding the winner at the end of the game and concluding with the score:
+
+if(humanScore > compScore){
+    alert(`You win the game! \n Human: ${humanScore} \n Computer: ${compScore}`);
+} else if (compScore > humanScore){
+    alert(`Computer wins the game! \n Human: ${humanScore} \n Computer: ${compScore}`)
+};
+
+
+
+
+
 }
 
-//Variables that will act as parameters for playRound()
-const humanSelection = getHumanChoice();
-const compSelection = getComputerChoice();
 
-playRound(humanSelection, compSelection);
+
+
+
+
+
+
+
+
