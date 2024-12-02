27a6affb295c0d24ee81186616ca28d24956cb46
Hatsunmikk <debos2003@gmail.com>
2024-12-02 14:20:45 +0530

    Add a function to get the human choice or move

--- logic.js
+++ logic.js
@@ -13,3 +13,21 @@ function getComputerChoice(){
 
     return computerChoice;
 };
+
+//Function to get a choice from the human
+function getHumanChoice(){
+    let humanChoice = prompt("Rock, Paper or Scissors?");
+
+    if(humanChoice === "Rock"){
+        alert("You chose Rock.")
+    } else if (humanChoice === "Paper"){
+        alert("You chose Paper.")
+    } else if (humanChoice === "Scissors"){
+        alert("You chose Scissors.")
+    } else {
+        alert("Please choose Rock, Paper or Scissors!");
+        getHumanChoice();  //Using recursion to guide the user back to the initial prompt if they enter an invalid choice
+    }
+
+    
+};
