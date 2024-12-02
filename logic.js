cf91d0e5e0a17bcb9b27a44848a6a3fb86029bc2
Hatsunmikk <debos2003@gmail.com>
2024-12-02 16:19:03 +0530

    Add function to play each round and decide the winner alongwith the score in the same round. Include a tie breaker within this function that redirects the flow to getHumanChoice() in the event of a tie

--- logic.js
+++ logic.js
@@ -1,4 +1,5 @@
 
+//Main function to play the game
 //Function to get a random choice from the computer
 function getComputerChoice(){
     let computerChoice = Math.random();
@@ -35,3 +36,42 @@ function getHumanChoice(){
 //Variables to keep track of the scores
 let compScore = 0;
 let humanScore = 0;
+
+//Function for each round
+function playRound (humanChoice, computerChoice){
+
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    //cases where the human wins
+    if(humanChoice === "rock" && computerChoice === "scissors"){
+        alert("You win! Rock beats Scissors.");
+        humanScore += 1;
+    } else if (humanChoice === "scissors" && computerChoice === "paper"){
+        alert("You win! Scissors beat Paper.");
+        humanScore += 1;
+    } else if (humanChoice === "paper" && computerChoice === "rock"){
+        alert("You win! Paper beats Rock.");
+        humanScore += 1;
+
+    } else if(computerChoice === "rock" && humanChoice === "scissors"){     //cases where the computer wins
+        alert("Computer wins! Rock beats Scissors.");
+        compScore += 1;
+    } else if(computerChoice === "scissors" && humanChoice === "paper"){    
+        alert("Computer wins! Scissors beat Paper.");
+        compScore += 1;
+    } else if(computerChoice === "paper" && humanChoice === "rock"){    
+        alert("Computer wins! Paper beats Rock.");
+        compScore += 1;
+    } else if(humanChoice === computerChoice){   //cases of tie
+        alert("It's a tie! Break it by choosing again.");
+        getHumanChoice();  //if there is a tie, break it by getting a new choice from the human, which starts a fresh round
+    }
+       
+}
+
+//Variables that will act as parameters for playRound()
+const humanSelection = getHumanChoice();
+const compSelection = getComputerChoice();
+
+playRound(humanSelection, compSelection);
